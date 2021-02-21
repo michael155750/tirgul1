f1246c2e5a45cdbb70f3a25d36f4b5a6fc4f8305
מיכאל <michael155750@gmail.com>
2021-02-21 14:25:39 +0200

    a

---
+++ tirgul1/Fibonacci.c
@@ -0,0 +1,8 @@
+#include "Fibonacci.h"
+#include <stdio.h>
+
+unsigned long long Fibonacci(size_t n)
+{
+
+    return 0;
+}
---
+++ tirgul1/Fibonacci.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/**
+ * @file "Fibonacci.h"
+ */
+
+/**
+ * @brief calculate the fibonacci number in the value n
+ *  using lazy evaluation
+ * @param n the serrial number of the fibonnaci number
+ * @return the fibonacci value
+*/
+unsigned long long Fibonacci(size_t n);
